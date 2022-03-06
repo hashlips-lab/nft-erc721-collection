fa117a122935ad2a6d39edb209341c3b8fe7eaa6
Marco Lipparini <1532277+liarco@users.noreply.github.com>
2022-03-07 00:27:05 +0100

    Supporting dynamic title in the DAPP (#73)

--- minting-dapp/src/scripts/main.tsx
+++ minting-dapp/src/scripts/main.tsx
@@ -2,6 +2,9 @@ import '../styles/main.scss';
 
 import ReactDOM from 'react-dom';
 import Dapp from './react/Dapp';
+import CollectionConfig from '../../../smart-contract/config/CollectionConfig';
+
+document.title = CollectionConfig.tokenName;
 
 document.addEventListener('DOMContentLoaded', async () => {
   ReactDOM.render(<Dapp />, document.getElementById('minting-dapp'));
