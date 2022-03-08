0c723c1c6eb90eb6281c66c9e83bb090f525c2a5
Marco Lipparini <1532277+liarco@users.noreply.github.com>
2022-03-08 10:25:16 +0100

    Do not set default DAPP title if it's already set (#93)
    * Do not set default DAPP title if it's already set
    
    * Fixing wrong step name

--- minting-dapp/src/scripts/main.tsx
+++ minting-dapp/src/scripts/main.tsx
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom';
 import Dapp from './react/Dapp';
 import CollectionConfig from '../../../smart-contract/config/CollectionConfig';
 
-document.title = CollectionConfig.tokenName;
+if (document.title === '') {
+  document.title = CollectionConfig.tokenName;
+}
 
 document.addEventListener('DOMContentLoaded', async () => {
   ReactDOM.render(<Dapp />, document.getElementById('minting-dapp'));
