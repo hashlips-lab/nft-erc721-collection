15048e6bf7395c3d6a5887d706c67d076950f572
Marco Lipparini <developer@liarco.net>
2022-09-23 13:18:46 +0200

    Switching to Goerli as official testnet for Ethereum (Rinkeby is now called as "ethereumLegacyTestnet")

--- smart-contract/lib/Networks.ts
+++ smart-contract/lib/Networks.ts
@@ -17,6 +17,16 @@ export const hardhatLocal: NetworkConfigInterface = {
  * Ethereum
  */
 export const ethereumTestnet: NetworkConfigInterface = {
+  chainId: 5,
+  symbol: 'ETH (test)',
+  blockExplorer: {
+    name: 'Etherscan (Goerli)',
+    generateContractUrl: (contractAddress: string) => `https://goerli.etherscan.io/address/${contractAddress}`,
+    generateTransactionUrl: (transactionAddress: string) => `https://goerli.etherscan.io/tx/${transactionAddress}`,
+  },
+}
+
+export const ethereumLegacyTestnet: NetworkConfigInterface = {
   chainId: 4,
   symbol: 'ETH (test)',
   blockExplorer: {
